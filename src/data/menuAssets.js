b65d85d790465422244678d2f9cd3712bf2aff83
ChronosBVRX <noirsysan@gmail.com>
2026-05-08 11:02:42 -0600

    FEAT: Second batch of AI menu images (Snacks and large portions)

--- src/data/menuAssets.js
+++ src/data/menuAssets.js
@@ -8,6 +8,14 @@ import aFenix6 from '../assets/menu/alitas_fenix_6pzs.png'
 import aFenix12 from '../assets/menu/alitas_fenix_12pzs.png'
 import bHipogrifo175 from '../assets/menu/boneless_hipogrifo_175gr.png'
 import bHipogrifo350 from '../assets/menu/boneless_hipogrifo_350gr.png'
+import bHipogrifo500 from '../assets/menu/boneless_hipogrifo_500gr.png'
+import aFenix18 from '../assets/menu/alitas_fenix_18pzs.png'
+import nuguetsHipo from '../assets/menu/nuguets_hipogrifo.png'
+import dedosQueso from '../assets/menu/dedos_queso.png'
+import palomitasAcro from '../assets/menu/palomitas_acromantula.png'
+import hClasico from '../assets/menu/hotdog_clasico.png'
+import hSalchichon from '../assets/menu/hotdog_salchichon.png'
+import pColacuerno from '../assets/menu/papas_colacuerno.png'
 
 export const MENU_ASSETS = {
   "Centauro Clásica": hCentauroClasica,
@@ -18,5 +26,13 @@ export const MENU_ASSETS = {
   "Alitas de Fénix,6 Pzs": aFenix6,
   "Alitas de Fénix,12 Pzs": aFenix12,
   "Boneless de Hipogrifo,175gr": bHipogrifo175,
-  "Boneless de Hipogrifo,350gr": bHipogrifo350
+  "Boneless de Hipogrifo,350gr": bHipogrifo350,
+  "Boneless de Hipogrifo,500gr": bHipogrifo500,
+  "Alitas de Fénix,18 Pzs": aFenix18,
+  "Nuguets 8 pzs. de Hipogrifo": nuguetsHipo,
+  "Dedos de queso 8 pzs. Elfo Domestico": dedosQueso,
+  "Palomitas de Acromantula (de maíz)": palomitasAcro,
+  "Hot Dog Clásico": hClasico,
+  "Hot Dog Con Salchichón": hSalchichon,
+  "Papas a la francesa Colacuerno": pColacuerno
 }
